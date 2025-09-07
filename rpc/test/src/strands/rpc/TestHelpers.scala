1811e480a1cb9d1b17ea094b1831c0a191168234
mushtaq <mushtaq.a@gmail.com>
2025-09-07 10:19:56 +0530

    place holder

---
+++ rpc/test/src/strands/rpc/TestHelpers.scala
@@ -0,0 +1,13 @@
+package strands.rpc
+
+import strands.rpc.common.{RpcBackend, RpcEndpoints}
+import sttp.client4.testing.StreamBackendStub
+import sttp.monad.IdentityMonad
+import sttp.tapir.server.stub4.TapirStreamStubInterpreter
+
+object TestHelpers:
+  extension (serverEndpoints: RpcEndpoints)
+    def backendStub(): RpcBackend =
+      TapirStreamStubInterpreter(StreamBackendStub(IdentityMonad))
+        .whenServerEndpointsRunLogic(serverEndpoints)
+        .backend()
